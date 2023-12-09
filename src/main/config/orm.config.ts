16a31b70db7baa4795bd2c7bf75d32c79e829a7b
Riquelme Maia Rodrigues <riquelmemaia2@gmail.com>
2023-12-09 19:34:24 -0300

    fix: update to the paths for executing migrations

--- src/main/config/orm.config.ts
+++ src/main/config/orm.config.ts
@@ -1,10 +1,10 @@
-import { DataSourceOptions } from "typeorm";
+import { DataSource } from "typeorm";
 import ENV from "../../app/envs/env";
 
 const env = ENV.Application.NODE_ENV;
 const basePath = env === "DEVELOPMENT" ? "src" : "dist";
 
-const ormConfig: DataSourceOptions = {
+const dataSource = new DataSource({
   type: "postgres",
   url: ENV.Database.DATABASE_URL,
   synchronize: false,
@@ -14,7 +14,7 @@ const ormConfig: DataSourceOptions = {
   },
   migrations: [`${basePath}/app/shared/database/migrations/**/*`],
   entities: [`${basePath}/app/shared/database/entities/**/*`],
-};
+})
 
-export { ormConfig };
+export { dataSource };
 
